8481684d44ac6a98e02a7d649c898eeca324cd7e
madhhuurrii <madhuriramakrishnan061@gmail.com>
2021-08-14 23:26:16 +0530

    update

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 
+// const covRoute = require('./routes/cov');
+
+//Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}))
+
+//Import Route 
 const covRoute = require('./routes/cov');
 
 //Use View Engine
 app.set('view engine','ejs')
 
 //Middleware route
-app.use('/test', covRoute);
+app.use('/', covRoute);
 
 const PORT = process.env.PORT || 5000;
 
--- routes/cov.js
+++ routes/cov.js
@@ -5,4 +5,9 @@ router.get('/',(req,res)=>{
     res.render('index');
 })
 
+router.post('/',(req,res)=>{
+    const city = req.body.city;
+
+
+})
 module.exports = router;
